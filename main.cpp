7ff1acd74c22353de71fa87b63d38cbdbc4e0795
MateoMagdaleno <22170@portalcedi.edu.mx>
2024-04-29 13:54:30 -0600

    1

--- main.cpp
+++ main.cpp
@@ -1,18 +1,9 @@
 #include <SFML/Graphics.hpp>
-#include "square.hpp"
-#include <vector>
 using namespace sf;
-using namespace std;
-
-Vector2f speed = {5.f, 10.f};
-
 int main()
 {
-    RenderWindow window(VideoMode(800, 600), "SFML works!");
-    Square shape(Vector2f(50.f, 50.f));
-    window.setFramerateLimit(30);
-    vector<Square> cuadrados;
-
+    RenderWindow window(VideoMode(200, 200), "SFML works!");
+    Grid grid(5, 5);
     while (window.isOpen())
     {
         Event event;
@@ -20,40 +11,10 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
-            if (event.type == Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == Mouse::Left)
-                {
-                    // int w = rand() % 100;
-                    // int h = rand() % 100;
-                    int w = 100;
-                    int h = 100;
-                    int x = event.mouseButton.x;
-                    int y = event.mouseButton.y;
-                    Square cuad(Vector2f(w, h), x, y);
-                    cuadrados.push_back(cuad);
-                }
-
-                if (event.mouseButton.button == Mouse::Right)
-                {
-                    int x = event.mouseButton.x;
-                    int y = event.mouseButton.y;
-                    for (auto &c : cuadrados)
-                    {
-                        c.click(x, y);
-                    }
-                }
-            }
         }
+
         window.clear();
-        shape.update();
-        shape.drawTo(window);
-        for (auto &c : cuadrados)
-        {
-            c.update();
-            c.drawTo(window);
-        }
-
+        grid.drawTo();
         window.display();
     }
 
